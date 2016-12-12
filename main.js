bd5c7f0e4864b8d2e5e75985fa7240f51bc3bc7c
Oslikovskyi <v.oslikovskiy@gmail.com>
2016-12-12 09:19:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var items1 = "# # # # #";
+var items2 = " # # # # #";
+var i = 0;
+while (i < 7) {
+if ( i % 2 == 0) {
+  console.log(items1);
+		}
+	else {
+    console.log(items2);
+	}
+    i++;
+}
